19bce663b21b3ec06e27b40b06c8c918ee2bcb3e
Aman Singh <amankumarsingh1412@gmail.com>
2022-02-27 14:45:53 +0530

    beryllium: Enable Blur

--- derp_beryllium.mk
+++ derp_beryllium.mk
@@ -10,6 +10,7 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 DERP_BUILDTYPE := Official
+TARGET_USES_BLUR := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := derp_beryllium
