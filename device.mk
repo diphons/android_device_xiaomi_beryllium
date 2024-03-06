a62c54c3c063ae0a1dfa1700241a8bdaf8bd6f70
Rudi Setiyawan <diphons@gmail.com>
2024-03-06 17:23:40 +0700

    beryllium: Derp-ify Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- device.mk
+++ device.mk
@@ -256,12 +256,10 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-derp
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
-
-PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
@@ -307,10 +305,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
-
-# PocketMode
-PRODUCT_PACKAGES += \
-    XiaomiPocketMode
 
 # Power
 PRODUCT_PACKAGES += \
